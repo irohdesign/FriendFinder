a1020d80f41e36559fdf0ac402337805df40d597
Jalia Evans <irohdesign@outlook.com>
2019-01-10 10:46:14 -0500

    input formatting for objects

---
+++ data/friends.js
@@ -0,0 +1,22 @@
+// app's data as an array of objects
+
+// format of objects in JSON
+
+{
+    "name" : "Ahmed",
+    "photo" : "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores" : [
+        5,
+        2,
+        1,
+        3,
+        4,
+        5,
+        3,
+        2,
+        3,
+        4,
+        6
+    ]
+}
+
