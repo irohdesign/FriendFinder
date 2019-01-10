07cc04dcf469db78c2ab4a0d92162449cfb17ead
Jalia Evans <irohdesign@outlook.com>
2019-01-10 12:04:32 -0500

    setting up paths to survey and home pages

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,6 +1,12 @@
 // include two routes
 
 // GET route to /survey which displays the survey page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/survey"))
+});
+
 
 // default route to home.html that displays the home page
-
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"))
+});
