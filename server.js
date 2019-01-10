08767cde0264befa60cd56e894fada3bdb0008f6
Jalia Evans <irohdesign@outlook.com>
2019-01-10 12:05:52 -0500

    setting up data parsing and paths for dependencies

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 // include express and path packages
 
-const app = require("express");
-const path = require("express");
+const express = require("express");
+const app = express();
+
+const path = require("path");
+
+// PORT for both local and heroku
+var PORT = process.env.PORT || 3000;
+
+// set up express to handle data parsing
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
 
 // convert user's results into simple array : [4, 5, 6, 7, 8, 8, etc]
 
