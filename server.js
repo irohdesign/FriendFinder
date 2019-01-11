6f4cc7324e2e79d09d3a1a117a255a2b30d6469c
Jalia Evans <irohdesign@outlook.com>
2019-01-10 23:19:02 -0500

    required object from friends and fixed up paths so the website works on the right pages. also started the server

--- server.js
+++ server.js
@@ -5,14 +5,38 @@ const app = express();
 
 const path = require("path");
 
+// require suitor info
+
+const suitor = require("./data/friends.js");
+
 // PORT for both local and heroku
 var PORT = process.env.PORT || 3000;
+
+
+console.log(suitors);
 
 // set up express to handle data parsing
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
+// routes
 
+// GET route to home which displays the home page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/home.html"))
+});
+
+
+// GET route to survey that displays the survey page
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/survey.html"))
+});
+
+// route to show all objects in suitors array
+
+app.get("/api/all", function(req, res) {
+    res.json(suitors);
+})
 
 // convert user's results into simple array : [4, 5, 6, 7, 8, 8, etc]
 
@@ -29,3 +53,10 @@ app.use(express.json());
     // modal displays both the name and picture of the closest match
     
     // create dynamic img with the src being the photo method of the user's object
+
+
+// start server
+
+app.listen(PORT, function() {
+    console.log(`Server is running at ${PORT}`);
+})
